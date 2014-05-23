18c39c0c56080f5d0c64cd6d768ee78e015fe469
mojixiang <moji_xiang@163.com>
2014-05-23 16:45:04 +0800

    mojixiang

--- public/js/editor_life.js
+++ public/js/editor_life.js
@@ -546,7 +546,7 @@ var LifeView = Backbone.View.extend({
         if(item.city_id==''||item.city_id==null || item.city_id==undefined){
             alert('城市不能为空！');
             $('#city_select').focus();
-            return false;
+            return true; 
         }
         
         if(this.model == null || this.model.get('_id') == null)
@@ -656,7 +656,10 @@ var LifeListView = Backbone.View.extend({
         var type = $('#life_type').val();
         var cityname =$('#search-life-cityname').val();
         var lifename =$('#search-life-name').val();
-        
+        var isLocalFlag = $('#local_flag').prop('checked'),
+            isMichilinFlag = $('#michilin_flag').prop('checked'),
+            isBestDinnerchoics = $('#best_dinnerchoics').prop('checked'),
+            isMostPopular = $('#most_popular').prop('checked');
         self.location = '/#lifes/1/'+type+'/q_'+cityname+'/q_'+encodeURIComponent(lifename);
     },
     selectType: function(){
