c82a70f009e82ffc61b3d5cc4ac50ed2e25afa1f
chanmd <470891965@qq.com>
2014-05-30 20:01:24 +0800

    data of path

---
+++ data/path/jiujinshan1.js
@@ -0,0 +1,601 @@
+exports.items = [{
+	"_id": "516cc44fe3c6a60f69000034",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.794209",
+	"longitude": "-122.406825"
+}, {
+	"_id": "516cc44fe3c6a60f69000037",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7865806193",
+	"longitude": "-122.401288748"
+}, {
+	"_id": "516cc44fe3c6a60f69000038",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.76872146",
+	"longitude": "-122.4412353"
+}, {
+	"_id": "516cc44fe3c6a60f69000039",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.8088600283",
+	"longitude": "-122.409292459"
+}, {
+	"_id": "516cc44fe3c6a60f6900003a",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.790612266",
+	"longitude": "-122.427172065"
+}, {
+	"_id": "516cc44fe3c6a60f6900003b",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7689400916",
+	"longitude": "-122.480823133"
+}, {
+	"_id": "516cc44fe3c6a60f6900003c",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.428831",
+	"longitude": "-122.169363"
+}, {
+	"_id": "516cc44fe3c6a60f6900003d",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.78782609",
+	"longitude": "-122.4039959"
+}, {
+	"_id": "516cc44fe3c6a60f6900003e",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.808934",
+	"longitude": "-122.418026"
+}, {
+	"_id": "516cc44fe3c6a60f6900003f",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.807894",
+	"longitude": "-122.408391"
+}, {
+	"_id": "516cc44fe3c6a60f69000040",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.79773",
+	"longitude": "-122.40248"
+}, {
+	"_id": "516cc44fe3c6a60f69000041",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7853977916",
+	"longitude": "-122.429650426"
+}, {
+	"_id": "516cc44fe3c6a60f69000042",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.8096652844",
+	"longitude": "-122.477356195"
+}, {
+	"_id": "516cc44fe3c6a60f69000044",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.794196",
+	"longitude": "-122.483246"
+}, {
+	"_id": "516cc44fe3c6a60f69000045",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7867332408",
+	"longitude": "-122.401192188"
+}, {
+	"_id": "516cc44fe3c6a60f69000046",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.79572112",
+	"longitude": "-122.3936422"
+}, {
+	"_id": "516cc44fe3c6a60f69000047",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80947",
+	"longitude": "-122.417"
+}, {
+	"_id": "516cc44fe3c6a60f69000048",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.797246",
+	"longitude": "-122.407914"
+}, {
+	"_id": "516cc44fe3c6a60f69000049",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80289391",
+	"longitude": "-122.4492339"
+}, {
+	"_id": "516cc44fe3c6a60f6900004a",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.77180027",
+	"longitude": "-122.4681187"
+}, {
+	"_id": "516cc44fe3c6a60f6900004b",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.785705",
+	"longitude": "-122.421317"
+}, {
+	"_id": "516cc44fe3c6a60f6900004c",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80592111",
+	"longitude": "-122.4229689"
+}, {
+	"_id": "516cc44fe3c6a60f6900004d",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.78605934",
+	"longitude": "-122.4014966"
+}, {
+	"_id": "516cc44fe3c6a60f6900004e",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.78674886",
+	"longitude": "-122.4323056"
+}, {
+	"_id": "516cc44fe3c6a60f6900004f",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.78520626",
+	"longitude": "-122.4297805"
+}, {
+	"_id": "516cc44fe3c6a60f69000050",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7701550033",
+	"longitude": "-122.47012496"
+}, {
+	"_id": "516cc44fe3c6a60f69000051",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7857751115",
+	"longitude": "-122.402136326"
+}, {
+	"_id": "516cc44fe3c6a60f69000052",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80140657",
+	"longitude": "-122.4209937"
+}, {
+	"_id": "516cc44fe3c6a60f69000053",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.78315291",
+	"longitude": "-122.4018735"
+}, {
+	"_id": "516cc44fe3c6a60f69000054",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7858259859",
+	"longitude": "-122.401245832"
+}, {
+	"_id": "516cc44fe3c6a60f69000055",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7801228",
+	"longitude": "-122.4160491"
+}, {
+	"_id": "516cc44fe3c6a60f69000056",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.76299212",
+	"longitude": "-122.420628"
+}, {
+	"_id": "516cc44fe3c6a60f69000057",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.8020785756",
+	"longitude": "-122.418792844"
+}, {
+	"_id": "516cc44fe3c6a60f69000058",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7594",
+	"longitude": "-122.510819"
+}, {
+	"_id": "516cc44fe3c6a60f69000059",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.784084",
+	"longitude": "-122.401458"
+}, {
+	"_id": "516cc44fe3c6a60f6900005a",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80586148",
+	"longitude": "-122.4550555"
+}, {
+	"_id": "516cc44fe3c6a60f6900005b",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.77923311",
+	"longitude": "-122.4193384"
+}, {
+	"_id": "516cc44fe3c6a60f6900005c",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.79521926",
+	"longitude": "-122.4027444"
+}, {
+	"_id": "516cc44fe3c6a60f6900005d",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.77639383",
+	"longitude": "-122.4348151"
+}, {
+	"_id": "516cc44fe3c6a60f6900005e",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.759231",
+	"longitude": "-122.411481"
+}, {
+	"_id": "516cc44fe3c6a60f6900005f",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.8267848359",
+	"longitude": "-122.422932344"
+}, {
+	"_id": "516cc44fe3c6a60f69000060",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.80295134",
+	"longitude": "-122.4484848"
+}, {
+	"_id": "51f87003b0c941b1110000fe",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.7544066",
+	"longitude": "-122.4476845"
+}, {
+	"_id": "51f87281b0c941b1110000ff",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.771469",
+	"longitude": "-122.4686755"
+}, {
+	"_id": "536a0b481713475210000003",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.788319",
+	"longitude": "-122.407333"
+}, {
+	"_id": "536b37f6171347521000001c",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.805851",
+	"longitude": "-122.410619"
+}, {
+	"_id": "536b3bb21713475210000022",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.802901",
+	"longitude": "-122.410058"
+}, {
+	"_id": "536b408a1713475210000031",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.800917",
+	"longitude": "-122.410058"
+}, {
+	"_id": "536b48981713475210000037",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.861234",
+	"longitude": "-122.432598"
+}, {
+	"_id": "536b4abb1713475210000038",
+	"type": 0,
+	"city_name": "旧金山",
+	"latitude": "37.800358",
+	"longitude": "-122.397998"
+}, {
+	"_id": "5322c1822617c8d426000001",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d426000002",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7585209631",
+	"longitude": "-122.392021545"
+}, {
+	"_id": "5322c1822617c8d426000003",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.729703",
+	"longitude": "-122.372124"
+}, {
+	"_id": "5322c1822617c8d426000004",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7901258",
+	"longitude": "-122.4092704"
+}, {
+	"_id": "5322c1822617c8d426000005",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7553579",
+	"longitude": "-122.42153"
+}, {
+	"_id": "5322c1822617c8d426000006",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7485824",
+	"longitude": "-122.4184108"
+}, {
+	"_id": "5322c1822617c8d426000007",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.806732466",
+	"longitude": "-122.405284725"
+}, {
+	"_id": "5322c1822617c8d426000008",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7543914",
+	"longitude": "-122.4112314"
+}, {
+	"_id": "5322c1822617c8d426000009",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d42600000a",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7971369",
+	"longitude": "-122.3996476"
+}, {
+	"_id": "5322c1822617c8d42600000b",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7776198",
+	"longitude": "-122.407065"
+}, {
+	"_id": "5322c1822617c8d42600000c",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7561438",
+	"longitude": "-122.4325682"
+}, {
+	"_id": "5322c1822617c8d42600000d",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7600592",
+	"longitude": "-122.388303"
+}, {
+	"_id": "5322c1822617c8d42600000e",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7968733883",
+	"longitude": "-122.39500165"
+}, {
+	"_id": "5322c1822617c8d42600000f",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.740908",
+	"longitude": "-122.465324"
+}, {
+	"_id": "5322c1822617c8d426000010",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7714817",
+	"longitude": "-122.422399"
+}, {
+	"_id": "5322c1822617c8d426000011",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.8017074",
+	"longitude": "-122.4120021"
+}, {
+	"_id": "5322c1822617c8d426000012",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.806732466",
+	"longitude": "-122.405284725"
+}, {
+	"_id": "5322c1822617c8d426000013",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d426000014",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7666622",
+	"longitude": "-122.4332944"
+}, {
+	"_id": "5322c1822617c8d426000015",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.8074698812",
+	"longitude": "-122.417274714"
+}, {
+	"_id": "5322c1822617c8d426000016",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7938587",
+	"longitude": "-122.4048565"
+}, {
+	"_id": "5322c1822617c8d426000017",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7901258",
+	"longitude": "-122.4092704"
+}, {
+	"_id": "5322c1822617c8d426000018",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7830597444",
+	"longitude": "-122.417336798"
+}, {
+	"_id": "5322c1822617c8d426000019",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7618242",
+	"longitude": "-122.3985871"
+}, {
+	"_id": "5322c1822617c8d42600001a",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7346189",
+	"longitude": "-122.4334337"
+}, {
+	"_id": "5322c1822617c8d42600001b",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d42600001c",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.787629",
+	"longitude": "-122.434031"
+}, {
+	"_id": "5322c1822617c8d42600001d",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7595785362",
+	"longitude": "-122.434720892"
+}, {
+	"_id": "5322c1822617c8d42600001e",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.8075970299",
+	"longitude": "-122.417328358"
+}, {
+	"_id": "5322c1822617c8d42600001f",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7528426",
+	"longitude": "-122.4100064"
+}, {
+	"_id": "5322c1822617c8d426000020",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.772869",
+	"longitude": "-122.416227"
+}, {
+	"_id": "5322c1822617c8d426000021",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7832586",
+	"longitude": "-122.4606845"
+}, {
+	"_id": "5322c1822617c8d426000022",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.750391",
+	"longitude": "-122.420149"
+}, {
+	"_id": "5322c1822617c8d426000023",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.775023",
+	"longitude": "-122.44427"
+}, {
+	"_id": "5322c1822617c8d426000024",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7775135",
+	"longitude": "-122.4381721"
+}, {
+	"_id": "5322c1822617c8d426000025",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7599762418",
+	"longitude": "-122.414875771"
+}, {
+	"_id": "5322c1822617c8d426000026",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d426000027",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.8075970299",
+	"longitude": "-122.417328358"
+}, {
+	"_id": "5322c1822617c8d426000028",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7227489",
+	"longitude": "-122.4353768"
+}, {
+	"_id": "5322c1822617c8d426000029",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.789045",
+	"longitude": "-122.4040583"
+}, {
+	"_id": "5322c1822617c8d42600002a",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7762804",
+	"longitude": "-122.4081578"
+}, {
+	"_id": "5322c1822617c8d42600002b",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7485824",
+	"longitude": "-122.4184108"
+}, {
+	"_id": "5322c1822617c8d42600002c",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7904248649",
+	"longitude": "-122.398536135"
+}, {
+	"_id": "5322c1822617c8d42600002d",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7677622",
+	"longitude": "-122.42829"
+}, {
+	"_id": "5322c1822617c8d42600002e",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.749495",
+	"longitude": "-122.4184706"
+}, {
+	"_id": "5322c1822617c8d42600002f",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.780448",
+	"longitude": "-122.4732808"
+}, {
+	"_id": "5322c1822617c8d426000030",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.75417",
+	"longitude": "-122.4131527"
+}, {
+	"_id": "5322c1822617c8d426000031",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.944847",
+	"longitude": "-122.505517"
+}, {
+	"_id": "5322c1822617c8d426000032",
+	"type": 1,
+	"city_name": "旧金山",
+	"latitude": "37.7811165",
+	"longitude": "-122.3953976"
+}];
